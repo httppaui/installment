be26d3cff49ca7cfc9e015397af9323e35a78b84
paui <163615470+httppaui@users.noreply.github.com>
2024-06-18 10:17:53 +0800

    Add files via upload

---
+++ 169_Pamittan_PaulaHewlett_Agcanas_JakeRussel/169_Pamittan_PaulaHewlett_Agcanas_JakeRussel/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace _169_Pamittan_PaulaHewlett_Agcanas_JakeRussel
+{
+    partial class frmMotor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblModel = new System.Windows.Forms.Label();
+            this.cboModel = new System.Windows.Forms.ComboBox();
+            this.txtCashValue = new System.Windows.Forms.TextBox();
+            this.lblCashValue = new System.Windows.Forms.Label();
+            this.lblInsurance = new System.Windows.Forms.Label();
+            this.txtInsurance = new System.Windows.Forms.TextBox();
+            this.lblRegistration = new System.Windows.Forms.Label();
+            this.txtRegistration = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMonthlyPayment = new System.Windows.Forms.TextBox();
+            this.lblPayment = new System.Windows.Forms.Label();
+            this.txtPayment = new System.Windows.Forms.TextBox();
+            this.chkInstallment = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(42, 40);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(91, 13);
+            this.lblModel.TabIndex = 0;
+            this.lblModel.Text = "Motorcycle Model";
+            // 
+            // cboModel
+            // 
+            this.cboModel.FormattingEnabled = true;
+            this.cboModel.Location = new System.Drawing.Point(45, 68);
+            this.cboModel.Name = "cboModel";
+            this.cboModel.Size = new System.Drawing.Size(134, 21);
+            this.cboModel.TabIndex = 1;
+            this.cboModel.SelectedIndexChanged += new System.EventHandler(this.cboModel_SelectedIndexChanged);
+            // 
+            // txtCashValue
+            // 
+            this.txtCashValue.Location = new System.Drawing.Point(229, 69);
+            this.txtCashValue.Name = "txtCashValue";
+            this.txtCashValue.ReadOnly = true;
+            this.txtCashValue.Size = new System.Drawing.Size(137, 20);
+            this.txtCashValue.TabIndex = 2;
+            // 
+            // lblCashValue
+            // 
+            this.lblCashValue.AutoSize = true;
+            this.lblCashValue.Location = new System.Drawing.Point(226, 40);
+            this.lblCashValue.Name = "lblCashValue";
+            this.lblCashValue.Size = new System.Drawing.Size(61, 13);
+            this.lblCashValue.TabIndex = 3;
+            this.lblCashValue.Text = "Cash Value";
+            // 
+            // lblInsurance
+            // 
+            this.lblInsurance.AutoSize = true;
+            this.lblInsurance.Location = new System.Drawing.Point(42, 117);
+            this.lblInsurance.Name = "lblInsurance";
+            this.lblInsurance.Size = new System.Drawing.Size(54, 13);
+            this.lblInsurance.TabIndex = 4;
+            this.lblInsurance.Text = "Insurance";
+            // 
+            // txtInsurance
+            // 
+            this.txtInsurance.Location = new System.Drawing.Point(229, 114);
+            this.txtInsurance.Name = "txtInsurance";
+            this.txtInsurance.ReadOnly = true;
+            this.txtInsurance.Size = new System.Drawing.Size(137, 20);
+            this.txtInsurance.TabIndex = 5;
+            // 
+            // lblRegistration
+            // 
+            this.lblRegistration.AutoSize = true;
+            this.lblRegistration.Location = new System.Drawing.Point(42, 148);
+            this.lblRegistration.Name = "lblRegistration";
+            this.lblRegistration.Size = new System.Drawing.Size(63, 13);
+            this.lblRegistration.TabIndex = 6;
+            this.lblRegistration.Text = "Registration";
+            // 
+            // txtRegistration
+            // 
+            this.txtRegistration.Location = new System.Drawing.Point(229, 145);
+            this.txtRegistration.Name = "txtRegistration";
+            this.txtRegistration.ReadOnly = true;
+            this.txtRegistration.Size = new System.Drawing.Size(137, 20);
+            this.txtRegistration.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Monthly Payment";
+            // 
+            // txtMonthlyPayment
+            // 
+            this.txtMonthlyPayment.Location = new System.Drawing.Point(229, 192);
+            this.txtMonthlyPayment.Name = "txtMonthlyPayment";
+            this.txtMonthlyPayment.Size = new System.Drawing.Size(137, 20);
+            this.txtMonthlyPayment.TabIndex = 9;
+            // 
+            // lblPayment
+            // 
+            this.lblPayment.AutoSize = true;
+            this.lblPayment.Location = new System.Drawing.Point(42, 226);
+            this.lblPayment.Name = "lblPayment";
+            this.lblPayment.Size = new System.Drawing.Size(48, 13);
+            this.lblPayment.TabIndex = 10;
+            this.lblPayment.Text = "Payment";
+            // 
+            // txtPayment
+            // 
+            this.txtPayment.Location = new System.Drawing.Point(229, 223);
+            this.txtPayment.Name = "txtPayment";
+            this.txtPayment.Size = new System.Drawing.Size(137, 20);
+            this.txtPayment.TabIndex = 11;
+            // 
+            // chkInstallment
+            // 
+            this.chkInstallment.AutoSize = true;
+            this.chkInstallment.Location = new System.Drawing.Point(229, 303);
+            this.chkInstallment.Name = "chkInstallment";
+            this.chkInstallment.Size = new System.Drawing.Size(76, 17);
+            this.chkInstallment.TabIndex = 12;
+            this.chkInstallment.Text = "Installment";
+            this.chkInstallment.UseVisualStyleBackColor = true;
+            this.chkInstallment.CheckedChanged += new System.EventHandler(this.chkInstallment_CheckedChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(291, 338);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(229, 252);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(137, 20);
+            this.txtTax.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Tax";
+            // 
+            // frmMotor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 404);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.chkInstallment);
+            this.Controls.Add(this.txtPayment);
+            this.Controls.Add(this.lblPayment);
+            this.Controls.Add(this.txtMonthlyPayment);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRegistration);
+            this.Controls.Add(this.lblRegistration);
+            this.Controls.Add(this.txtInsurance);
+            this.Controls.Add(this.lblInsurance);
+            this.Controls.Add(this.lblCashValue);
+            this.Controls.Add(this.txtCashValue);
+            this.Controls.Add(this.cboModel);
+            this.Controls.Add(this.lblModel);
+            this.Name = "frmMotor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Motorcycle Installment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.ComboBox cboModel;
+        private System.Windows.Forms.TextBox txtCashValue;
+        private System.Windows.Forms.Label lblCashValue;
+        private System.Windows.Forms.Label lblInsurance;
+        private System.Windows.Forms.TextBox txtInsurance;
+        private System.Windows.Forms.Label lblRegistration;
+        private System.Windows.Forms.TextBox txtRegistration;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMonthlyPayment;
+        private System.Windows.Forms.Label lblPayment;
+        private System.Windows.Forms.TextBox txtPayment;
+        private System.Windows.Forms.CheckBox chkInstallment;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 169_Pamittan_PaulaHewlett_Agcanas_JakeRussel/169_Pamittan_PaulaHewlett_Agcanas_JakeRussel/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _169_Pamittan_PaulaHewlett_Agcanas_JakeRussel
+{
+    public partial class frmMotor : Form
+    {
+
+        double tax1 = 0.03;
+        double tax2 = 0.11;
+        double ADVinstallment = 274840.00;
+        double PCXinstallment = 250400.00;
+        double CLICKinstallment = 153800.00;
+        double ADVcash = 168800.00;
+        double PCXcash = 151800.00;
+        double CLICKcash = 82800.00;
+        double insurance = 4500.00;
+        double registration = 3100.00;
+        int months = 36;
+        double total = 0.0;
+        double payment = 0.0;
+        double taxDeduction = 0.0;
+        double taxTotal = 0.0;
+
+        public frmMotor()
+        {
+            InitializeComponent();
+
+         
+    
+            ComboBox cBox = new ComboBox();
+
+
+            cboModel.Items.Add("ADV");
+            cboModel.Items.Add("PCX");
+            cboModel.Items.Add("CLICK");
+            Controls.Add(cboModel);
+
+            cboModel.SelectedIndexChanged += cboModel_SelectedIndexChanged;
+        }
+
+        private void cboModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboModel.SelectedIndex >= 0)
+            {
+                string selectedText = cboModel.SelectedItem.ToString();
+
+                switch (selectedText)
+                {
+                    case "ADV":
+                        txtCashValue.Text = ADVcash.ToString();
+                        break;
+
+                    case "PCX":
+                        txtCashValue.Text = PCXcash.ToString();
+                        break;
+
+                    case "CLICK":
+                        txtCashValue.Text = CLICKcash.ToString();
+                        break;
+
+                    default:
+                        txtCashValue.Text = " ";
+                        break;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (chkInstallment.Checked == false)
+            {
+            
+
+                txtMonthlyPayment.Text = "Fully Paid";
+                txtInsurance.Text = insurance.ToString();
+                txtRegistration.Text = registration.ToString();
+
+                double taxDeduction = Double.Parse(txtCashValue.Text) * tax1;
+                txtTax.Text = taxDeduction.ToString();
+
+               
+
+            }
+            else
+            {
+
+                string selectedText = cboModel.SelectedItem.ToString();
+
+                switch (selectedText)
+                {
+                    case "ADV":
+                       
+                         taxDeduction = ADVinstallment * tax2;
+                         taxTotal = ADVinstallment + taxDeduction;
+
+                        txtMonthlyPayment.Text = (((taxTotal - Double.Parse(txtPayment.Text)) / months).ToString());
+
+                        txtInsurance.Text = insurance.ToString();
+                        txtRegistration.Text = registration.ToString();
+                        txtTax.Text = taxDeduction.ToString();
+                        break;
+
+                    case "PCX":
+
+                         taxDeduction = PCXinstallment * tax2;
+                         taxTotal = PCXinstallment + taxDeduction;
+
+                        txtMonthlyPayment.Text = (((taxTotal - Double.Parse(txtPayment.Text)) / months).ToString());
+
+                        txtInsurance.Text = insurance.ToString();
+                        txtRegistration.Text = registration.ToString();
+                        txtTax.Text = taxDeduction.ToString();
+                        break;
+
+                    case "CLICK":
+
+                         taxDeduction = CLICKinstallment * tax2;
+                         taxTotal = CLICKinstallment + taxDeduction;
+
+                        txtMonthlyPayment.Text = (((taxTotal - Double.Parse(txtPayment.Text)) / months).ToString());
+
+                        txtInsurance.Text = insurance.ToString();
+                        txtRegistration.Text = registration.ToString();
+                        txtTax.Text = taxDeduction.ToString();
+                        break;
+                }
+            }
+        }
+
+        private void chkInstallment_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkInstallment.Checked)
+            {
+                txtPayment.ReadOnly = false;
+            }
+
+            else
+            {
+                txtPayment.ReadOnly = true;
+            }
+        }
+    }
+}
